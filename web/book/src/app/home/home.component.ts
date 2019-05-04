0a9a9989130b19f826ce872da1224249e6b67243
mil-ast <mil-ast@yandex.ru>
2019-05-04 20:35:31 +0300

    кнопки oauth

--- web/book/src/app/home/home.component.ts
+++ web/book/src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { OauthService } from './service/oauth.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+  styleUrls: ['./home.component.scss'],
   providers: [
     OauthService,
   ]
