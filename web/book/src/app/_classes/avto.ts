ddc053bd7075a7206ee040655efcdd47c731f2f6
mil-ast <mil-ast@yandex.ru>
2019-09-12 21:20:33 +0300

    upd

--- web/book/src/app/_classes/avto.ts
+++ web/book/src/app/_classes/avto.ts
@@ -1,44 +1,42 @@
 export interface AvtoStruct {
-    avto_id?: number;
-    user_id?: number;
-    name: string;
-    odo: number;
-    avatar?: boolean;
-    public?: boolean;
+  avto_id?: number;
+  user_id?: number;
+  name: string;
+  odo: number;
+  avatar?: boolean;
+  public?: boolean;
 }
 
 export class Avto {
-    avto_id: number;
-    name: string;
-    odo: number;
-    avatar: boolean;
-    private cache: number;
+  avto_id: number;
+  name: string;
+  odo: number;
+  avatar: boolean;
+  private cache: number;
 
-    constructor(data: AvtoStruct) {
-        this.avto_id = data.avto_id;
-        this.update(data.name, data.odo, data.avatar);
-    }
+  constructor(data: AvtoStruct) {
+    this.avto_id = data.avto_id;
+    this.update(data.name, data.odo, data.avatar);
+  }
 
-    update(name: string, odo: number, avatar: boolean) {
-        this.name = name;
-        this.odo = odo;
-        this.avatar = avatar;
-        this.cache = Date.now();
-    }
+  update(name: string, odo: number, avatar: boolean) {
+    this.name = name;
+    this.odo = odo;
+    this.avatar = avatar;
+    this.cache = Date.now();
+  }
 
-    avatarPath(type: string): string {
-        if (!this.avatar) {
-            if (type === 'medium') {
-                return '/assets/images/no_photo.png';
-            } else {
-                return '/assets/images/no_photo_small.png';
-            }
-        } else {
-            if (type === 'medium') {
-                return `/api/images/medium/${this.avto_id}.jpg?t=${this.cache}`;
-            } else {
-                return `/api/images/small/${this.avto_id}.jpg?t=${this.cache}`;
-            }
-        }
+  get avatarSmallPath(): string {
+    if (!this.avatar) {
+      return '/assets/images/no_photo_small.png';
     }
+    return `/api/images/small/${this.avto_id}.jpg?t=${this.cache}`;
+  }
+
+  get avatarMediumPath(): string {
+    if (!this.avatar) {
+      return '/assets/images/no_photo.png';
+    }
+    return `/api/images/medium/${this.avto_id}.jpg?t=${this.cache}`;
+  }
 }
--- web/book/src/app/shared/list-avto/list-avto.component.ts
+++ web/book/src/app/shared/list-avto/list-avto.component.ts
@@ -5,25 +5,27 @@ import { DialogCreateAvtoComponent } from '../dialog-create-avto/dialog-create-a
 import { AvtoService } from 'src/app/_services/avto.service';
 import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation-dialog.component';
 import { DialogUpdateAvtoComponent } from '../dialog-update-avto/dialog-update-avto.component';
-import { ScreenService, Screen, SmallScreen } from 'src/app/_services/screen.service';
-import { ReplaySubject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { ReplaySubject, of } from 'rxjs';
+import { takeUntil, mergeMap, map } from 'rxjs/operators';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-list-avto',
   templateUrl: './list-avto.component.html',
-  styleUrls: ['../../shared_styles/aside.scss', './list-avto.component.scss'],
+  styleUrls: [
+    '../../shared_styles/aside.scss',
+    '../../shared_styles/aside_list.scss',
+    './list-avto.component.scss'
+  ],
 })
 export class ListAvtoComponent implements OnInit, OnDestroy {
   selectedAvto: Avto = null;
   avtoList: Avto[] = [];
 
-  private smallScreen = false;
   private destroy: ReplaySubject<boolean> = new ReplaySubject(1);
 
   constructor(
     private avtoService: AvtoService,
-    private screenService: ScreenService,
     private dialog: MatDialog,
   ) { }
 
@@ -34,23 +36,20 @@ export class ListAvtoComponent implements OnInit, OnDestroy {
       this.selectedAvto = avto;
     });
 
-    this.screenService.getScreen().pipe(
-      takeUntil(this.destroy)
-    ).subscribe(this.configureSideNav.bind(this));
     this.fetchAvto();
   }
 
   ngOnDestroy() {
     this.destroy.next(null);
     this.destroy.complete();
+    this.dialog.closeAll();
   }
 
   clickShowAddAvto(disableClose: boolean = false) {
-    const dialog = this.dialog.open(DialogCreateAvtoComponent, {
+    this.dialog.open(DialogCreateAvtoComponent, {
       autoFocus: false,
       disableClose: disableClose,
-    });
-    dialog.afterClosed().subscribe((data: AvtoStruct) => {
+    }).afterClosed().subscribe((data: AvtoStruct) => {
       if (data) {
         const avto = new Avto(data);
         this.avtoList.push(avto);
@@ -59,52 +58,48 @@ export class ListAvtoComponent implements OnInit, OnDestroy {
     });
   }
 
-  clickShowEditAvto(avto: AvtoStruct) {
-    let position = null;
-    if (!this.smallScreen) {
-      position = '40px';
-    }
+  clickShowEditAvto() {
     const dialog = this.dialog.open(DialogUpdateAvtoComponent, {
       autoFocus: false,
-      data: avto,
+      data: this.selectedAvto,
       width: '500px',
-      position: {
-        left: position,
-      }
     });
     dialog.afterClosed().subscribe();
   }
 
-  clickDeleteAvto(avto: Avto) {
-    const dialog = this.dialog.open(ConfirmationDialogComponent, {
+  clickDeleteAvto() {
+    this.dialog.open(ConfirmationDialogComponent, {
       data: {
         title: 'Удалить авто?',
-        message: 'Удалить авто и всю историю по ней? Восстановление будет невозможным!',
+        message: `Удалить ${this.selectedAvto.name} и всю историю по ней? Восстановление будет невозможным!`,
         type: 'warn'
       },
-    });
-    dialog.afterClosed().subscribe((ok: boolean) => {
+    }).afterClosed().pipe(
+      mergeMap((ok: boolean) => {
+        return ok ? this.avtoService.delete(this.selectedAvto.avto_id).pipe(map(() => true)) : of(false);
+      })
+    ).subscribe((ok: boolean) => {
       if (!ok) {
         return;
       }
-
-      this.avtoService.delete(avto.avto_id).subscribe(() => {
-        this.onAvtoDelete(avto);
-      });
+      const index = this.avtoList.indexOf(this.selectedAvto);
+      if (index !== -1) {
+        this.avtoList.splice(index, 1);
+        if (this.avtoList.length > 0) {
+          this.avtoService.setSelected(this.avtoList[0]);
+        } else {
+          this.avtoService.setSelected(null);
+        }
+      }
+    }, (err: HttpErrorResponse) => {
+      // TODO
+      // this.
     });
   }
 
-  // евент удаления авто
-  onAvtoDelete(avto: Avto) {
-    const index = this.avtoList.indexOf(avto);
-    if (index !== -1) {
-      this.avtoList.splice(index, 1);
-      if (this.avtoList.length > 0) {
-        this.avtoService.setSelected(this.avtoList[0]);
-      } else {
-        this.avtoService.setSelected(null);
-      }
-    }
+  clickSelectAvto(avto: Avto, event: MouseEvent) {
+    event.preventDefault();
+    this.avtoService.setSelected(avto);
   }
 
   private fetchAvto() {
@@ -116,9 +111,5 @@ export class ListAvtoComponent implements OnInit, OnDestroy {
         this.clickShowAddAvto(true);
       }
     });
-  }
-
-  private configureSideNav(screen: Screen) {
-    this.smallScreen = screen.innerWidth < SmallScreen ? true : false;
   }
 }
