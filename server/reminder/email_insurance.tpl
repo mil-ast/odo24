25181969c396fc5c15b20319eb14037710063565
mil-ast <mil-ast@yandex.ru>
2019-12-25 21:11:26 +0300

    удалено старое, поправлен eml шаблоны

--- server/reminder/email_insurance.tpl
+++
@@ -1,5 +0,0 @@
-To: %s
-Content-Type: text/html
-Subject: Напоминание с Odometer.online
-
-Вы создавали напоминание на сервисе Odometer.online и <b>%s</b> дней закончится ваша страховка!<br/>
--- server/sendmail/sendmail.go
+++ server/sendmail/sendmail.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"html/template"
 	"io/ioutil"
+	"log"
 	"odo24/server/config"
 
 	email "github.com/mil-ast/sendmail"
@@ -22,23 +23,24 @@ var templates map[uint8]string
 func init() {
 	templates = make(map[uint8]string)
 
+	files := map[uint8]string{
+		TypeConfirmEmail:      "confirm_email",
+		TypeRepairConfirmCode: "confirm_repair_code",
+		TypeRegisterFromOAUTH: "oauth_register",
+	}
+
 	var (
 		body []byte
 		err  error
 	)
-	// TypeConfirmEmail
-	body, err = ioutil.ReadFile("./sendmail/confirm_email.eml")
-	if err != nil {
-		panic(err)
-	}
-	templates[TypeConfirmEmail] = string(body)
 
-	// TypeRepairConfirmCode
-	body, err = ioutil.ReadFile("./sendmail/confirm_repair_code.eml")
-	if err != nil {
-		panic(err)
+	for index, fileName := range files {
+		body, err = ioutil.ReadFile(fmt.Sprintf("./sendmail/%s.eml", fileName))
+		if err != nil {
+			panic(err)
+		}
+		templates[index] = string(body)
 	}
-	templates[TypeRepairConfirmCode] = string(body)
 }
 
 // SendEmail отправка
@@ -63,6 +65,8 @@ func SendEmail(to string, tplID uint8, params map[string]interface{}) error {
 	}
 
 	templateBody = fmt.Sprintf(templateBody, options.App.SmtpFrom, to)
+
+	log.Println(777, templateBody)
 
 	buffer := new(bytes.Buffer)
 	t := template.Must(template.New("letter").Parse(templateBody))
